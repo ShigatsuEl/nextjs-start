26af263b4d74e3c9c591694885f137d6967613ee
MinChan Lee <shigatsu970704@gmail.com>
2022-01-21 14:21:05 +0900

    [Feat] 타사 자바스크립트

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
 
 /**
  * 구성 요소의 이름은 제한이 없지만 내보내기로 내보내야 합니다 default.
@@ -10,6 +11,13 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
