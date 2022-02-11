98d3016e464277f195dbb0e4c69894f2bd1aa818
MinChan Lee <shigatsu970704@gmail.com>
2022-02-11 19:37:13 +0900

    [Feat] 연마 레이아웃

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -8,7 +8,7 @@ import Layout from '../../components/layout'
  */
 export default function FirstPost() {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>First Post</title>
       </Head>
